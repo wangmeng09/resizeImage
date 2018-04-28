06746e210693091c47e89158e88c7a249fdbba70
nfc-wm <wangmeng@gyj.com>
2018-04-28 11:30:25 +0800

    框架文档

--- README.md
+++
@@ -1,2 +0,0 @@
-# resizeImage
-图片压缩上传及预览
---
+++ src/global/index.js
@@ -0,0 +1,16 @@
+// 全局插件，提供全局操作函数
+import Filter from './filter'
+import Directive from './directive'
+import JSLoader from './js-loader'
+import CSSLoader from './css-loader'
+import AsyncComponent from './async-component'
+import CallbackHook from './hook'
+
+export {
+  Filter,
+  Directive,
+  JSLoader,
+  CSSLoader,
+  AsyncComponent,
+  CallbackHook
+}
